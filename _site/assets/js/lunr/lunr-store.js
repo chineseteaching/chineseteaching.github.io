3d9750d6e9a8fa10a766ab325518044cee1a56fb
chineseteaching <37094155+chineseteaching@users.noreply.github.com>
2018-03-09 23:36:46 -0800

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "Edge Case: Nested and Mixed Lists",
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough.Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item ordered...","categories": ["Edge Case"],
+        "tags": ["content","css","edge case","lists","markup"],
+        "url": "http://localhost:4000/edge%20case/edge-case-nested-and-mixed-lists/",
+        "teaser":null},{
+        "title": "Edge Case: Many Tags",
+        "excerpt":"This post has many tags.","categories": ["Edge Case"],
+        "tags": ["8BIT","alignment","Articles","captions","categories","chat","comments","content","css","dowork","edge case","embeds","excerpt","Fail","featured image","FTW","Fun","gallery","html","image","Jekyll","layout","link","Love","markup","Mothership","Must Read","Nailed It","Pictures","Post Formats","quote","standard","Success","Swagger","Tags","template","title","twitter","Unseen","video","YouTube"],
+        "url": "http://localhost:4000/edge%20case/edge-case-many-tags/",
+        "teaser":null},{
+        "title": "Edge Case: Many Categories",
+        "excerpt":"This post has many categories.","categories": ["aciform","antiquarianism","arrangement","asmodeus","broder","buying","championship","chastening","disinclination","disinfection","dispatch","echappee","enphagy"],
+        "tags": ["categories","edge case"],
+        "url": "http://localhost:4000/aciform/antiquarianism/arrangement/asmodeus/broder/buying/championship/chastening/disinclination/disinfection/dispatch/echappee/enphagy/edge-case-many-categories/",
+        "teaser":null},{
+        "title": "Edge Case: No Body Content",
+        "excerpt":"","categories": ["Edge Case"],
+        "tags": ["content","edge case","layout"],
+        "url": "http://localhost:4000/edge%20case/edge-case-no-body-content/",
+        "teaser":null},{
+        "title": "hello jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll"],
+        "tags": ["jekyll"],
+        "url": "http://localhost:4000/jekyll/welcome-to-jekyll/",
+        "teaser":null}]
