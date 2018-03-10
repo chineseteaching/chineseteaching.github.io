1af2ab70df85c1ec0b43fa66253c042feb70f9f5
chineseteaching <37094155+chineseteaching@users.noreply.github.com>
2018-03-09 23:51:12 -0800

    s

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,26 +1,36 @@
-var store = [{
-        "title": "Edge Case: Nested and Mixed Lists",
-        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough.Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item ordered...","categories": ["Edge Case"],
-        "tags": ["content","css","edge case","lists","markup"],
-        "url": "http://localhost:4000/edge%20case/edge-case-nested-and-mixed-lists/",
-        "teaser":null},{
-        "title": "Edge Case: Many Tags",
-        "excerpt":"This post has many tags.","categories": ["Edge Case"],
-        "tags": ["8BIT","alignment","Articles","captions","categories","chat","comments","content","css","dowork","edge case","embeds","excerpt","Fail","featured image","FTW","Fun","gallery","html","image","Jekyll","layout","link","Love","markup","Mothership","Must Read","Nailed It","Pictures","Post Formats","quote","standard","Success","Swagger","Tags","template","title","twitter","Unseen","video","YouTube"],
-        "url": "http://localhost:4000/edge%20case/edge-case-many-tags/",
-        "teaser":null},{
-        "title": "Edge Case: Many Categories",
-        "excerpt":"This post has many categories.","categories": ["aciform","antiquarianism","arrangement","asmodeus","broder","buying","championship","chastening","disinclination","disinfection","dispatch","echappee","enphagy"],
-        "tags": ["categories","edge case"],
-        "url": "http://localhost:4000/aciform/antiquarianism/arrangement/asmodeus/broder/buying/championship/chastening/disinclination/disinfection/dispatch/echappee/enphagy/edge-case-many-categories/",
-        "teaser":null},{
-        "title": "Edge Case: No Body Content",
-        "excerpt":"","categories": ["Edge Case"],
-        "tags": ["content","edge case","layout"],
-        "url": "http://localhost:4000/edge%20case/edge-case-no-body-content/",
-        "teaser":null},{
-        "title": "hello jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll"],
-        "tags": ["jekyll"],
-        "url": "http://localhost:4000/jekyll/welcome-to-jekyll/",
-        "teaser":null}]
+---
+layout: null
+---
+
+var store = [
+  {%- for c in site.collections -%}
+    {%- if forloop.last -%}
+      {%- assign l = true -%}
+    {%- endif -%}
+    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
+    {%- for doc in docs -%}
+      {%- if doc.header.teaser -%}
+        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
+      {%- else -%}
+        {%- assign teaser = site.teaser -%}
+      {%- endif -%}
+      {
+        "title": {{ doc.title | jsonify }},
+        "excerpt":
+          {%- if site.search_full_content == true -%}
+            {{ doc.content | strip_html | strip_newlines | jsonify }},
+          {%- else -%}
+            {{ doc.content | strip_html | strip_newlines | truncatewords: 50 | jsonify }},
+          {%- endif -%}
+        "categories": {{ doc.categories | jsonify }},
+        "tags": {{ doc.tags | jsonify }},
+        "url": {{ doc.url | absolute_url | jsonify }},
+        "teaser":
+          {%- if teaser contains "://" -%}
+            {{ teaser | jsonify }}
+          {%- else -%}
+            {{ teaser | absolute_url | jsonify }}
+          {%- endif -%}
+      }{%- unless forloop.last and l -%},{%- endunless -%}
+    {%- endfor -%}
+  {%- endfor -%}]
---
+++ pages/callforpaper.html
@@ -0,0 +1,16 @@
+---
+layout: archive
+author_profile: true
+
+excerpt: "Join us at **University of California Santa Barbara** for teaching and researching in the area of Chinese language"
+header: 
+
+ overlay_image: /assets/images/Kitten.png
+ <!--overlay_filter: 0.2 # same as adding an opacity of 0.5 to a black background-->
+ overlay_filter: rgba(500, 0, 0, 0.2)
+
+---
+<!--{% include sidebar.html %}-->
+<div>
+asdlifjasidfj 
+</div>
